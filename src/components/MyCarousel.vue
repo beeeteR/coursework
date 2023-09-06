a8b640f4dc29f89ab2a81de21ae8e1296342b717
beeeteR <130251761+beeeteR@users.noreply.github.com>
2023-09-06 17:06:39 +0400

    1

---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                class="carousel-btn active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" class="carousel-btn"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" class="carousel-btn"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/10.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/11.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/12.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.carousel-item {
+    height: 600px;
+
+    img {
+        object-fit: cover;
+        max-height: 100%;
+        height: auto;
+    }
+}
+
+.carousel-indicators {
+    gap: 1rem;
+    .carousel-btn {
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+    }
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container home-content">
+    <my-carousel></my-carousel>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MyCarousel from "@/components/MyCarousel.vue";
+
+</script>
+
+
+<style lang="scss" scoped>
+.line {
+  height: 2px;
+  width: 100%;
+  margin-top: 10px;
+  border-radius: 50%;
+  background: linear-gradient(90deg, rgb(255, 255, 255), rgb(185, 185, 185) 5%, rgb(145, 145, 145), rgb(185, 185, 185) 95%, rgb(255, 255, 255));
+}
+
+</style>
