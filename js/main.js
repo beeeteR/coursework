34647164e633fdd9dfce1799af5e3b08f7b3170c
beeeteR <130251761+beeeteR@users.noreply.github.com>
2023-10-19 03:11:06 +0400

    1

--- js/main.js
+++ js/main.js
@@ -2,12 +2,63 @@ const catalogBtn = document.getElementById('catalog-arrow')
 const catalog = document.querySelector('.catalog')
 const btnSVG = catalogBtn.querySelector('svg')
 
+const btnLeft = document.querySelector('.popular__arrow.--left')
+const btnRight = document.querySelector('.popular__arrow.--right')
+const popularItems = document.querySelectorAll('.popular__item')
+
+
 catalogBtn.addEventListener('click', () => {
     if (catalog.classList.contains('expand')) {
         catalog.classList.remove('expand')
         btnSVG.style.transform = ''
-    }else {
+    } else {
         catalog.classList.add('expand')
         btnSVG.style.transform = 'rotate(180deg)'
     }
 })
+
+
+
+btnRight.addEventListener('click', () => {
+    changeActivePopularItem(null, 1)
+})
+btnLeft.addEventListener('click', () => {
+    changeActivePopularItem(null, -1)
+})
+popularItems.forEach(el => {
+    el.addEventListener('click', () => {
+        changeActivePopularItem(el)
+    })
+})
+
+function changeActivePopularItem(item = null, direction = 1) {
+    for (let i = 0; i < popularItems.length; i++) {
+        const el = popularItems[i];
+
+        if (el.classList.contains('--item-active')) {
+            el.classList.remove('--item-active')
+
+            if (item === null) {
+                switch (direction) {
+                    case 1:
+                        if (i === popularItems.length - 1) {
+                            popularItems[0].classList.add('--item-active')
+                        } else {
+                            popularItems[i + 1].classList.add('--item-active')
+                        }
+                        break;
+                    case -1:
+                        if (i === 0) {
+                            popularItems[popularItems.length - 1].classList.add('--item-active')
+                        } else {
+                            popularItems[i - 1].classList.add('--item-active')
+                        }
+                        break;
+                }
+                break
+            } else {
+                item.classList.add('--item-active')
+            }
+        }
+    }
+}
